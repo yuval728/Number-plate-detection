ebd1b098d67543fb4359c516d06a434b91c362d4
Yuval728 <yuvalmehta.728@gmail.com>
2024-08-24 13:08:30 +0530

    added ocr and upadte the app

--- app/app.py
+++ app/app.py
@@ -1,20 +1,38 @@
-from ultralytics import YOLO
-import gradio as gr
 import cv2
 import numpy as np
+from ultralytics import YOLO
+import easyocr
+import gradio as gr
+import json
 
 
-number_plate_model = YOLO("best.pt")
+number_plate_model = YOLO("best.pt", task='detection')
+reader = easyocr.Reader(['en'])
 
 def detect_number_plate(image):
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = number_plate_model(image)
+    for result in results:
+        result = json.loads(result.tojson()) 
+        
+        x1, y1, x2, y2 = map(int, result[0]['box'].values())
+        # print(x1, y1, x2, y2)
+        
+        number_plate = image[y1:y2, x1:x2]
+        number_plate = cv2.cvtColor(number_plate, cv2.COLOR_BGR2RGB)
+        
+        number_plate_text = reader.readtext(number_plate)
+        if len(number_plate_text) > 0:
+            number_plate_text = number_plate_text[0][-2]
+        else:
+            number_plate_text = "No text detected"
+        
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(image, number_plate_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
     
-    return results[0].plot( )
+    return image
 
-
+# Define Gradio interface
 image = gr.Image(height=640, width=640, type="numpy")
 label = gr.Image(height=640, width=640, type="numpy")
 
 gr.Interface(fn=detect_number_plate, inputs=image, outputs=label).launch(debug=True, share=False)
-
